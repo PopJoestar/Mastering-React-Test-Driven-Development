462d0cb80eebc7bf82eb06c7a905662d98203f83
gaetan <andrianjafygaetan@gmail.com>
2022-09-26 21:52:45 +0300

    Add Time slot table 's header

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,6 +1,64 @@
 import React, { useState } from "react";
 
-export const AppointmentForm = ({ selectableServices, service, onSubmit }) => {
+const timeIncrements = (numTimes, startTime, increment) =>
+  Array(numTimes)
+    .fill([startTime])
+    .reduce((acc, _, i) => acc.concat([startTime + i * increment]));
+
+const dailyTimeSlots = (salonOpensAt, salonClosesAt) => {
+  const totalSlots = (salonClosesAt - salonOpensAt) * 2;
+  const startTime = new Date().setHours(salonOpensAt, 0, 0, 0);
+  const increment = 30 * 60 * 1000;
+  return timeIncrements(totalSlots, startTime, increment);
+};
+
+const weeklyDateValues = (startDate) => {
+  const midnight = new Date(startDate).setHours(0, 0, 0, 0);
+  const increment = 24 * 60 * 60 * 1000;
+  return timeIncrements(7, midnight, increment);
+};
+
+const toTimeValue = (timestamp) =>
+  new Date(timestamp).toTimeString().substring(0, 5);
+
+const toShortDate = (timestamp) => {
+  const [day, , dayOfMonth] = new Date(timestamp).toDateString().split(" ");
+  return `${day} ${dayOfMonth}`;
+};
+
+const TimeSlotTable = ({ salonOpensAt, salonClosesAt, today }) => {
+  const timeSlots = dailyTimeSlots(salonOpensAt, salonClosesAt);
+  const dates = weeklyDateValues(today);
+
+  return (
+    <table id="time-slots">
+      <thead>
+        <tr>
+          <th />
+          {dates.map((date) => (
+            <th key={date}>{toShortDate(date)}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {timeSlots.map((timeSlot) => (
+          <tr key={timeSlot}>
+            <th>{toTimeValue(timeSlot)}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export const AppointmentForm = ({
+  selectableServices,
+  service,
+  onSubmit,
+  salonOpensAt,
+  salonClosesAt,
+  today,
+}) => {
   const [appointment, setAppointment] = useState({ service });
 
   const handleChange = ({ target }) => {
@@ -24,6 +82,11 @@ export const AppointmentForm = ({ selectableServices, service, onSubmit }) => {
           <option key={s}>{s}</option>
         ))}
       </select>
+      <TimeSlotTable
+        salonOpensAt={salonOpensAt}
+        salonClosesAt={salonClosesAt}
+        today={today}
+      />
     </form>
   );
 };
@@ -37,4 +100,7 @@ AppointmentForm.defaultProps = {
     "Cut & beard trim",
     "Extensions",
   ],
+  salonOpensAt: 9,
+  salonClosesAt: 19,
+  today: new Date(),
 };
--- appointments/test/AppointmentForm.test.js
+++ appointments/test/AppointmentForm.test.js
@@ -10,11 +10,11 @@ describe("AppointmentForm", () => {
   const field = (name) => form("appointment").elements[name];
   const labelFor = (formElement) =>
     container.querySelector(`label[for="${formElement}"]`);
-
   const findOption = (dropdownNode, textContent) => {
     const options = Array.from(dropdownNode.childNodes);
     return options.find((option) => option.textContent == textContent);
   };
+  const timeSlotTable = () => container.querySelector("table#time-slots");
 
   beforeEach(() => {
     ({ render, container } = createContainer());
@@ -112,6 +112,46 @@ describe("AppointmentForm", () => {
       act(() => {
         ReactTestUtils.Simulate.submit(form("appointment"));
       });
+    });
+  });
+
+  describe("time slot table", () => {
+    it("renders a table for time slots", () => {
+      render(<AppointmentForm />);
+
+      expect(timeSlotTable).not.toBeNull();
+    });
+
+    it("renders a time slot for every half an hour between open and close times", () => {
+      render(<AppointmentForm salonOpensAt={9} salonClosesAt={11} />);
+      const timesOfDay = timeSlotTable().querySelectorAll("tbody >* th");
+      expect(timesOfDay).toHaveLength(4);
+      expect(timesOfDay[0].textContent).toEqual("09:00");
+      expect(timesOfDay[1].textContent).toEqual("09:30");
+      expect(timesOfDay[3].textContent).toEqual("10:30");
+    });
+
+    it("renders an empty cell at the start of the header row", () => {
+      render(<AppointmentForm />);
+
+      const headerRow = timeSlotTable().querySelector("thead > tr");
+
+      expect(headerRow.firstChild.textContent).toEqual("");
+    });
+
+    it("renders a week of available dates", () => {
+      const today = new Date(2022, 8, 24);
+
+      render(<AppointmentForm today={today} />);
+
+      const dates = timeSlotTable().querySelectorAll(
+        "thead >* th:not(:first-child)"
+      );
+
+      expect(dates).toHaveLength(7);
+      expect(dates[0].textContent).toEqual("Sat 24");
+      expect(dates[1].textContent).toEqual("Sun 25");
+      expect(dates[6].textContent).toEqual("Fri 30");
     });
   });
 });
